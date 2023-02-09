ef91f30d161d98115bbb3747b231e7ea7415fa7e
Valentin Erdyakov <valentin@Mac-mini-Valentin.local>
2023-02-10 03:26:17 +0500

    fix

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(`${__dirname}/dist`));
 
-app.get("/:slug", function(_req,res) {
-  res.sendFile(__dirname + '/dist/index.html');
+app.get('*', (_, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 app.listen(PORT, () => {
